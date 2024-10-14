cabf2f46e37b63272c8f01731440def6bfda7c33
Md Ashikur Rahman <ashik4339@gmail.com>
2024-10-14 22:03:39 +0600

    update anition

---
+++ assets/js/plugin.js
@@ -0,0 +1,11 @@
+import "../css/plugin.css";
+import "../css/styles.css";
+import "aos/dist/aos.css";
+import AOS from "aos";
+AOS.init({
+  duration: 800, // Animation duration
+  easing: "ease-in-out", // Easing function
+  once: true, // Whether animation should happen only once
+});
+import "preline";
+import "./custom";
